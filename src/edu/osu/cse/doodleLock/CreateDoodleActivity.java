995596c835f5d84ed163454a4a29ff30e5f6a832
Alex Notwell <alex.notwell@gmail.com>
2012-11-25 15:54:14 -0500

    Added multiple attempts and ui changes

--- src/edu/osu/cse/doodleLock/CreateDoodleActivity.java
+++ src/edu/osu/cse/doodleLock/CreateDoodleActivity.java
@@ -16,117 +16,229 @@
 
 package edu.osu.cse.doodleLock;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import android.app.Activity;
-import android.os.Bundle;
-import android.os.Environment;
-import android.view.View;
-import android.view.MotionEvent;
-import android.gesture.GestureOverlayView;
 import android.gesture.Gesture;
 import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
-
-import edu.osu.cse.doodleLock.R;
-
-public class CreateDoodleActivity extends Activity {
+public class CreateDoodleActivity extends Activity
+{
+    private static final int TRAINING_SESSION_COUNT_MAX = 10;
+    private static final int TRAINING_SESSION_COUNT_MIN = 3;
     private static final float LENGTH_THRESHOLD = 120.0f;
 
-    private Gesture mGesture;
-    private View mDoneButton;
+    protected Gesture mGesture;
+
+    /**
+     * List of the gestures that the user has saved
+     */
+    protected ArrayList<Gesture> mSavedGestureList;
+
+    /**
+     * The button that is selected in order to finish the training session
+     */
+    protected View mFinishSessionButton;
+
+    /**
+     * The button that is selected in order to discard the doodle from the training session
+     */
+    protected View mDiscardDoodleButton;
+
+    /**
+     * The button that is selected in order to save a doodle
+     */
+    protected View mSaveDoodleButton;
+
+    /**
+     * The view on which to draw gestures
+     */
+    protected GestureOverlayView mGestureOverlay;
+    
+    /**
+     * The doodle that is being used to authenticate the user and verify the training gestures
+     */
+    protected Doodle mDoodle;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.create_gesture);
 
-        mDoneButton = findViewById(R.id.done);
+        // Assign button layout elements to member variables
+        mFinishSessionButton = findViewById(R.id.finishSession);
+        mDiscardDoodleButton = findViewById(R.id.discardDoodle);
+        mSaveDoodleButton = findViewById(R.id.saveDoodle);
+        mGestureOverlay = (GestureOverlayView) findViewById(R.id.gestures_overlay);
+        
+        // Disable the Finish button until the min number of doodles have been trained
+        mFinishSessionButton.setEnabled(false);
+        mDiscardDoodleButton.setEnabled(false);
+        mSaveDoodleButton.setEnabled(false);
+        
+        // Set the doodle as null for the first gesture
+        mDoodle = null;
+        
+        mGestureOverlay.addOnGestureListener(new GesturesProcessor());
 
-        GestureOverlayView overlay = (GestureOverlayView) findViewById(R.id.gestures_overlay);
-        overlay.addOnGestureListener(new GesturesProcessor());
+        mSavedGestureList = new ArrayList<Gesture>();
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
+    protected void onSaveInstanceState(Bundle outState)
+    {
         super.onSaveInstanceState(outState);
-        
-        if (mGesture != null) {
+
+        if (mGesture != null)
+        {
             outState.putParcelable("gesture", mGesture);
         }
     }
 
     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
         super.onRestoreInstanceState(savedInstanceState);
-        
+
         mGesture = savedInstanceState.getParcelable("gesture");
-        if (mGesture != null) {
-            final GestureOverlayView overlay =
-                    (GestureOverlayView) findViewById(R.id.gestures_overlay);
+        if (mGesture != null)
+        {
+            final GestureOverlayView overlay = (GestureOverlayView) findViewById(R.id.gestures_overlay);
             overlay.post(new Runnable() {
-                public void run() {
+                public void run()
+                {
                     overlay.setGesture(mGesture);
                 }
             });
 
-            mDoneButton.setEnabled(true);
+            mFinishSessionButton.setEnabled(true);
         }
     }
 
-    @SuppressWarnings({"UnusedDeclaration"})
-    public void addGesture(View v) {
-        if (mGesture != null) {
+    @SuppressWarnings({ "UnusedDeclaration" })
+    public void onSaveButtonPress(View v)
+    {
+        if (mGesture != null)
+        {
+            mSaveDoodleButton.setEnabled(false);
+            mDiscardDoodleButton.setEnabled(false);
+            
             final TextView input = (TextView) findViewById(R.id.gesture_name);
             final CharSequence name = input.getText();
-            if (name.length() == 0) {
+            if (name.length() == 0)
+            {
                 input.setError(getString(R.string.error_missing_name));
                 return;
             }
 
             final GestureLibrary store = GestureBuilderActivity.getStore();
-            store.addGesture(name.toString(), mGesture);
-            store.save();
 
             setResult(RESULT_OK);
 
-            final String path = new File(Environment.getExternalStorageDirectory(),
-                    "gestures").getAbsolutePath();
-            Toast.makeText(this, getString(R.string.save_success, path), Toast.LENGTH_LONG).show();
-        } else {
+            final String path = new File(Environment.getExternalStorageDirectory(), "gestures").getAbsolutePath();
+            
+            boolean haveMinDoodlesBeenDrawn = mSavedGestureList.size() >= TRAINING_SESSION_COUNT_MIN;
+            boolean haveMaxDoodlesBeenDrawn = mSavedGestureList.size() >= TRAINING_SESSION_COUNT_MAX;
+            
+            // If the max number of sessions has not been met
+            if (!haveMaxDoodlesBeenDrawn)
+            {
+                // Don't authenticate attempts up to the minimum
+                if (!haveMinDoodlesBeenDrawn)
+                {
+                    mSavedGestureList.add(mGesture);
+                    mDoodle = new Doodle(mSavedGestureList);
+                    store.addGesture(name.toString().concat("" + mSavedGestureList.size()), mGesture);
+                    store.save();
+                    Toast.makeText(this, getString(R.string.save_success, path), Toast.LENGTH_SHORT).show();
+                }
+                // Authenticate subsequent doodles
+                else if (haveMinDoodlesBeenDrawn)
+                {
+                    // If the doodle matches the training set
+                    if (mDoodle.authenticate(mGesture))
+                    {
+                        mSavedGestureList.add(mGesture);
+                        mDoodle = new Doodle(mSavedGestureList);   
+                        store.addGesture(name.toString().concat("" + mSavedGestureList.size()), mGesture);
+                        store.save();
+                        Toast.makeText(this, getString(R.string.save_success, path), Toast.LENGTH_SHORT).show();
+                    }
+                    // Doodle failed to authenticate
+                    else
+                    {
+                        // Alert the user to try again
+                        Toast.makeText(this, getString(R.string.invalid_gesture, path), Toast.LENGTH_LONG).show();                        
+                    }
+                }                
+            }
+            // If the min number of session has been met
+            if (mSavedGestureList.size() >= TRAINING_SESSION_COUNT_MIN)
+            {
+                mFinishSessionButton.setEnabled(true);
+            }
+            mGestureOverlay.clear(false);
+        }
+        else
+        {
             setResult(RESULT_CANCELED);
         }
-
-        finish();
-        
     }
     
-    @SuppressWarnings({"UnusedDeclaration"})
-    public void cancelGesture(View v) {
+    @SuppressWarnings({ "UnusedDeclaration" })
+    public void onDiscardButtonPress(View v)
+    {
         setResult(RESULT_CANCELED);
+        mGestureOverlay.clear(false);
+    }
+
+    public void onFinishSessionButtonPress(View v)
+    {
+        //TODO What to do with the Doodle that has been created?
         finish();
     }
-    
-    private class GesturesProcessor implements GestureOverlayView.OnGestureListener {
-        public void onGestureStarted(GestureOverlayView overlay, MotionEvent event) {
-            mDoneButton.setEnabled(false);
+
+    private class GesturesProcessor implements GestureOverlayView.OnGestureListener
+    {
+        public void onGestureStarted(GestureOverlayView overlay, MotionEvent event)
+        {
+            mSaveDoodleButton.setEnabled(false);
+            mDiscardDoodleButton.setEnabled(false);
             mGesture = null;
         }
 
-        public void onGesture(GestureOverlayView overlay, MotionEvent event) {
+        public void onGesture(GestureOverlayView overlay, MotionEvent event)
+        {
         }
 
-        public void onGestureEnded(GestureOverlayView overlay, MotionEvent event) {
+        public void onGestureEnded(GestureOverlayView overlay, MotionEvent event)
+        {
             mGesture = overlay.getGesture();
-            if (mGesture.getLength() < LENGTH_THRESHOLD) {
+            if (mGesture.getLength() < LENGTH_THRESHOLD)
+            {
                 overlay.clear(false);
+                mGesture = null;
             }
-            mDoneButton.setEnabled(true);
+            else
+            {
+                mSaveDoodleButton.setEnabled(true);
+                mDiscardDoodleButton.setEnabled(true);
+            }
+
         }
 
-        public void onGestureCancelled(GestureOverlayView overlay, MotionEvent event) {
+        public void onGestureCancelled(GestureOverlayView overlay, MotionEvent event)
+        {
         }
     }
 }
